3343ad7c02b77d8faa3c671bd3d7f22b15c66afb
William Yaworsky <will.yaworsky@gmail.com>
2015-09-24 23:20:27 -0400

    initial commit

---
+++ globals.js
@@ -0,0 +1,37 @@
+GLOBAL.path     = require('path');
+GLOBAL.fs       = require('fs');
+
+GLOBAL.ROOT     = path.join(process.env.PWD, 'data');
+GLOBAL.noop     = function() {};
+
+GLOBAL.chalk    = require('chalk');
+
+GLOBAL.inquirer = require('inquirer');
+GLOBAL.db       = require('diskdb');
+
+GLOBAL.model    = require('./lib/model');
+GLOBAL.state    = require('./lib/state');
+
+try {
+  // disable stdout for a time because diskdb prints stuff to stdout whether we
+  // want it to or not
+  var oldWrite         = process.stdout.write;
+  process.stdout.write = noop;
+
+  var modelCollections = [];
+  var models = fs.readdirSync(path.join(__dirname, 'lib', 'models'));
+  for (var i in models) {
+    var moduleName = models[i].replace(/\.js$/, '');
+    GLOBAL[moduleName] = require('./lib/models/' + moduleName);
+    modelCollections.push(GLOBAL[moduleName].collectionName);
+  }
+
+  process.stdout.write = oldWrite;
+} catch (ex) {
+
+}
+
+GLOBAL.settings = Setting.all().reduce(function(acc, setting) {
+  acc[setting.data.name] = setting;
+  return acc;
+}, {});
